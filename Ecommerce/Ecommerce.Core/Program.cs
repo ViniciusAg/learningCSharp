5712f454d7869083f5df49e67867b80ae70a6a32
Vinicis Aguiar <aguiar.santos.vinicius@gmail.com>
2020-03-24 12:43:19 -0300

    Criando Classe para Cadastro de Cliente

---
+++ Ecommerce/Ecommerce.Core/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ecommerce.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vinicius = new Cadastro(nome: "Vinicius Aguiar", cpf: "123.456.789/12", idade: 22);
+            vinicius.Endereço = "Rua Coronel Pereira Neto";
+            vinicius.Telefone = "(11) 95023-4862";
+            vinicius.Email = "aguiar.santos.vinicius@gmail.com";
+          
+            vinicius.MostrarCadastro(vinicius);
+            Console.ReadLine();
+        }
+    }
+}
